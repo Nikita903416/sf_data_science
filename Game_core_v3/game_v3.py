991d6cafd722f50dbc92c8dd2b160b82f0a74403
Nikita <ionov.nikita.serg@gmail.com>
2023-10-06 19:11:48 +0300

    Initial commit

---
+++ Game_core_v3/game_v3.py
@@ -0,0 +1,48 @@
+"""Угадай число """
+
+import numpy as np
+
+def game_core_v3(number: int = 1) -> int:
+    """Сначала устанавливаем любое random число, а потом уменьшаем
+    или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток
+       
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    predict = np.random.randint(1, 101)
+    
+    while number != predict:
+        count += 1
+        if number > predict:
+            predict += 5
+        elif number < predict:
+            predict -= 4
+
+    return count
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
+    
+print('Run benchmarking for game_core_v3: ', end='')
+score_game(game_core_v3)
